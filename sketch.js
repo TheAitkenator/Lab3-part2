fc699fefffe6085edd38b36907b9059d9327750d
TheAitkenator <d.aitken1@rgu.ac.uk>
2024-09-19 16:43:00 +0100

    first commit

---
+++ sketch.js
@@ -0,0 +1,26 @@
+
+//Task 2 Number Month
+function setup() {
+let data = prompt("Please enter a number between 1 and 12 which is equal to a season in the year:");
+data = Number(data);
+
+if(data <0 ||  data > 12){
+  console.log("That is not a valid answer")
+}else if(data == 1 || data == 2 || data == 12){
+  console.log("Your Season is Winter:");
+}else if(data == 3 || data == 4 || data == 5){
+  console.log("Your Season is Spring:");
+}else if(data == 6 || data == 7 || data == 8){
+  console.log("Your Season is Summer:");
+}else if(data == 9 || data == 10 || data == 11){
+  console.log("Your Season is Autumn:");
+}
+
+createCanvas(400, 400);
+
+
+}
+
+function draw() {
+  background(220);
+}
